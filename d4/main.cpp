97e2a4fe2bd94a1ec16b01743a63ba1fff86f038
Damian Reiter <damian.reiter@gmail.com>
2022-12-03 21:34:58 -0800

    minor

--- d4/main.cpp
+++ d4/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int p1(vector<string> input) {
   int res = 0;
   for (auto &s : input) {
-    auto [r, l1, h1, l2, h2] = scn::scan_tuple<int, int, int, int>(s, "{}-{},{}-{}");
+    auto [_, l1, h1, l2, h2] = scn::scan_tuple<int, int, int, int>(s, "{}-{},{}-{}");
     if ((l1 <= l2 && h1 >= h2) || (l2 <= l1 && h2 >= h1)) {
       ++res;
     }
@@ -17,7 +17,7 @@ int p1(vector<string> input) {
 int p2(vector<string> input) {
   int res = 0;
   for (auto &s : input) {
-    auto [r, l1, h1, l2, h2] = scn::scan_tuple<int, int, int, int>(s, "{}-{},{}-{}");
+    auto [_, l1, h1, l2, h2] = scn::scan_tuple<int, int, int, int>(s, "{}-{},{}-{}");
     if ((min(l1, l2) == l1 && h1 >= l2) || (min(l1, l2) == l2 && h2 >= l1)) {
       ++res;
     }
